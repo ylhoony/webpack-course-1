e4c294abb5520e96c46fcbc93b49c136a3bd62e4
Hoon Lee <yl.hoony@gmail.com>
2017-05-01 22:29:20 -0700

    webpack basic setting

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  }
+};
+
+module.exports = config;
