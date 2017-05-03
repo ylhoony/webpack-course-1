16cd6c391562844dd645f75d5c6620f566d45d29
Hoon Lee <yl.hoony@gmail.com>
2017-05-02 23:55:40 -0700

    webpack code splitting basic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 // const sum = require('./sum');
-import sum from './sum';
-import './image_viewer';
+// import sum from './sum';
+// import './image_viewer';
+//
+// const total = sum(10, 5);
+// console.log(total);
 
-const total = sum(10, 5);
-console.log(total);
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
+  System.import('./image_viewer').then(module => {
+    // console.log('module:', module);
+    module.default();
+  });
+};
+
+document.body.appendChild(button);
